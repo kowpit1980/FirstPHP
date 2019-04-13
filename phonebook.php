e2ede75ffa31eff65f8053727e450bad79faa77c
Piotr Kowalewski <kowpit1980@gmail.com>
2019-04-13 16:39:30 +0200

    :))))

--- phonebook.php
+++ phonebook.php
@@ -11,4 +11,22 @@ die ("No connection". mysqli_error());
 if(!mysqli_select_db($dbh,"phone_book")){
 die ("Unable to connect to database" . mysqli_error());
 }
+
+$sql_stmt = "SELECT * FROM phonebook";
+$result = mysqli_query($dbh, $sql_stmt);
+
+if(!$result){
+    die ("Unable to execute query");
+}
+
+$rows = mysqli_num_rows($result);
+
+if ($rows) {
+while ($row = mysqli_fetch_array($result)){
+    echo 'ID' .$row['id'] . '<br>';
+    echo 'name' .$row['name'] . '<br>';
+}
+
+}
+mysqli_close($dbh);
 ?>
