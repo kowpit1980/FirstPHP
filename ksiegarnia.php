54b8f976cb20645d00e70bab6dcb747b7b4fb546
Piotr Kowalewski <kowpit1980@gmail.com>
2019-04-13 12:57:12 +0200

    ok

--- ksiegarnia.php
+++ ksiegarnia.php
@@ -8,7 +8,8 @@ $fizyka = new Book("Mark Twain","teksańska masakra piłą" );
 $matematyka = new Book("Lolly Holly", "Kaszanka na deser");
 $chemia = new Book("Belly Dancer", "Ala ma kota");
 
-echo $fizyka -> author. "<br>";
-echo $fizyka -> title;
-
+echo $fizyka -> author. " - ";
+echo $fizyka -> title. "<br>";
+echo $matematyka -> author. " - ";
+echo $matematyka -> title. "<br>";
 ?>
