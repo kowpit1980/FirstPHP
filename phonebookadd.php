7045cb368bb8ccc0abd5e0b9c2971f4606e1c8f9
Piotr Kowalewski <kowpit1980@gmail.com>
2019-04-14 10:31:02 +0200

    php progress

---
+++ phonebookadd.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>"> 
+<label for="name">Please enter your name: </label> 
+<input name="name" type="text"><br> 
+<label for="email">Please enter your email: </label> 
+<input name="email" type="text"><br> 
+<label for="mobile">Please enter your mobile: </label> 
+<input name="mobile" type="text"><br> 
+<input type="submit" value="Submit", name="submitForm"> 
+</form> 
+<?php
+if(isset($_POST["submitForm"])){
+
+
+$nm=$_POST['name']; 
+$email=$_POST['email']; 
+$mobile=$_POST['mobile']; 
+
+
+
+require ("db.php");
+$dbh=connectDb(); 
+
+	//connect to MySQL server if (!$dbh)     
+    
+    if (!$dbh){
+   die ("No connection". mysqli_error());
+        
+  }
+
+	if (!mysqli_select_db($dbh,"phone_book")) {   
+	die("Unable to select database: " . mysql_error()); 
+	//if selection fails output error message 
+    }
+	$sql_stmt = "INSERT INTO `phonebook` (`name`,`email`,`mobile`)"; 
+	$sql_stmt .= " VALUES('$nm',' $email ','$mobile')"; 
+    
+	$result = mysqli_query($dbh,$sql_stmt); //execute SQL statement 
+    
+	if (!$result)   {  
+        mysqli_close($dbh);
+        die("Adding record failed: " . mysqli_error()); 
+    }
+		//output error message if query execution failed echo "Poseidon has been successfully added to your contacts list";
+        
+ mysqli_close($dbh); //close the database connection
+} 
+?>
+</body> 
+</html> 
