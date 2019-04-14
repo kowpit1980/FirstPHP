b939c62b342d66026a372f893c86acb78b50224b
Piotr Kowalewski <kowpit1980@gmail.com>
2019-04-14 12:43:52 +0200

    progress

--- phonebookrecord.php
+++ phonebookrecord.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html> 
-<html> 
-<body> 
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>"> 
-<label for="name">Please enter your name: </label> 
-<input name="name" type="text"><br> 
-<label for="email">Please enter your email: </label> 
-<input name="email" type="text"><br> 
-<label for="mobile">Please enter your mobile: </label> 
-<input name="mobile" type="text"><br> 
-<input type="submit" value="Submit", name="submitForm"> 
-</form> 
+
+
 <?php
-require ("db.php");
 
+require ("db.php");
 if(isset($_POST["submitForm"])){
     $nm=$_POST['name']; 
     $email=$_POST['email']; 
@@ -41,21 +31,23 @@ if(isset($_POST["submitForm"])){
     }
 
     mysqli_close($dbh);
-}
-//output error message if query execution failed echo "Poseidon has been successfully added to your contacts list";
+} else {
+ 
+$id = $_GET['id'];
 
 $dbh=connectDb();
 
+
 if (!$dbh){
     die ("No connection". mysqli_error());
-}
 
+}
 if(!mysqli_select_db($dbh,"phone_book")){
     mysqli_close($dbh);
     die ("Unable to connect to database" . mysqli_error());
 }
 
-$sql_stmt = "SELECT * FROM phonebook";
+$sql_stmt = "SELECT * FROM phonebook WHERE id=$id" ;
 $result = mysqli_query($dbh, $sql_stmt);
 
 if(!$result){
@@ -67,14 +59,29 @@ $rows = mysqli_num_rows($result);
 
 if ($rows) {
     while ($row = mysqli_fetch_array($result)){
-        echo '<a href="/teb/phonebookrecord.php?id=' .$row['id'] . '">' .$row['id'] . '</a><br>';
-        echo 'name' .$row['name'] . '<br>';
-        // echo 'email' .$row['email'] . '<br>';
-        //  echo 'mobile' .$row['mobile'] . '<br>'; 
+    echo 'ID' .$row['id'] . '<br>';
+    echo 'name' .$row['name'] . '<br>';
+    echo 'email' .$row['email'] . '<br>';
+    echo 'mobile' .$row['mobile'] . '<br>';
+    ?>
+        
+     <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>"> 
+        <label for="name">Please enter your name: </label> 
+        <input name="name" type="text" value="<?php echo $row['name'];?>"><br> 
+        <label for="email">Please enter your email: </label> 
+        <input name="email" type="text"  value="<?php echo $row['email'];?>"><br> 
+        <label for="mobile">Please enter your mobile: </label> 
+        <input name="mobile" type="text"  value="<?php echo $row['mobile'];?>"><br> 
+        <input type="submit" value="Submit", name="submitForm"> 
+        <input name="id" hidden type="text"  value="<?php echo $row['id'];?>"><br> 
+
+        </form> 
+<?php
     }
 }
-    
+        
 mysqli_close($dbh); //close the database connection
+
+}
+
 ?>
-</body> 
-</html> 
